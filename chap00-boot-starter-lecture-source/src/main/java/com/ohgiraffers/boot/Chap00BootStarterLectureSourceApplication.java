8569f57245ff3a384d5c80d39ea7d46d85636e70
rlaskarb <rlaskarb20@gmail.com>
2024-11-13 20:32:56 +0900

    11/13

---
+++ chap00-boot-starter-lecture-source/src/main/java/com/ohgiraffers/boot/Chap00BootStarterLectureSourceApplication.java
@@ -0,0 +1,13 @@
+package com.ohgiraffers.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Chap00BootStarterLectureSourceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Chap00BootStarterLectureSourceApplication.class, args);
+    }
+
+}
---
+++ chap00-boot-starter-lecture-source/src/test/java/com/ohgiraffers/boot/Chap00BootStarterLectureSourceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ohgiraffers.boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Chap00BootStarterLectureSourceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
